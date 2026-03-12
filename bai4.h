d79535b0936b36c6de93ac262035ddab70c1d7b6
Yuno-20EA <haidzusuk@gmail.com>
2026-03-12 17:00:55 +0700

    Update bai4.h

--- bai4.h
+++ bai4.h
@@ -2,12 +2,12 @@
 // Created by OS on 3/12/2026.
 //
 
-#ifndef VJU_EXERCISES_BAI4_H
-#define VJU_EXERCISES_BAI4_H
+#ifndef BT_BAI4_H
+#define BT_BAI4_H
 
 
 class bai4 {
 };
 
 
-#endif //VJU_EXERCISES_BAI4_H
+#endif 
