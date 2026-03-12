1d3f70a2286d89c20e936836e27defda0f0b4bf7
Hoang Hai <haidzusuk@gmail.com>
2026-03-12 16:58:22 +0700

    done

---
+++ bai3.h
@@ -0,0 +1,13 @@
+//
+// Created by OS on 3/12/2026.
+//
+
+#ifndef UNTITLED4_BAI3_H
+#define UNTITLED4_BAI3_H
+
+
+class bai3 {
+};
+
+
+#endif //UNTITLED4_BAI3_H
---
+++ bai4.h
@@ -0,0 +1,13 @@
+//
+// Created by OS on 3/12/2026.
+//
+
+#ifndef VJU_EXERCISES_BAI4_H
+#define VJU_EXERCISES_BAI4_H
+
+
+class bai4 {
+};
+
+
+#endif //VJU_EXERCISES_BAI4_H
